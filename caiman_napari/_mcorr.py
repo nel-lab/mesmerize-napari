7517ae6c7c70130f1eac48e7ae7d9ca13ceeccbb
ArjunPutcha <arjun22@live.unc.edu>
2022-01-13 22:15:28 -0500

    save mmap file path to output in df series, function to load output added

--- caiman_napari/_mcorr.py
+++ caiman_napari/_mcorr.py
@@ -36,11 +36,22 @@ def main(batch_path, uuid):
     fnames = [str(input_movie_path)]
     mc = MotionCorrect(fnames, dview = dview, **opts.get_group('motion'))
     mc.motion_correct(save_movie = True)
-    np.save(input_movie_path + 'mc.npy', mc.mmap_file)
+    ix = df[df['uuid'] == uuid].index[0]
+    df['outputs'][ix] = mc.mmap_file
+    # Add the Series to the DataFrame
+    print('map file', df['outputs'][ix])
+    print('new df', df)
+    # Save DataFrame to disk
+    df.to_pickle(batch_path)
+    #np.save(input_movie_path + 'mc.npy', mc.mmap_file)
 
-def load_output_mcorr(viewer, batch_path, uuid):
+def load_output_mcorr(viewer, batch_item: pd.Series):
     print("output mcorr movie")
-
+    path = batch_item['outputs'][0]
+    print(path)
+    Yr, dims, T = cm.load_memmap(path)
+    MotionCorrectedMovie = np.reshape(Yr.T, [T] + list(dims), order='F')
+    viewer.add_image(MotionCorrectedMovie)
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
