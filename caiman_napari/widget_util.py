65224f20956f3f08759a77400e1b4ca09f6f0168
ArjunPutcha <arjun22@live.unc.edu>
2022-01-30 22:30:59 -0500

    util file to plot matplotlib widgets, modified from mesmerize code

---
+++ caiman_napari/widget_util.py
@@ -0,0 +1,23 @@
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from PyQt5 import QtWidgets
+from matplotlib.figure import Figure
+
+class MatplotlibWidget(QtWidgets.QWidget):
+    def __init__(self, size=(5.0, 4.0), dpi=100):
+        QtWidgets.QWidget.__init__(self)
+        self.fig = Figure(size, dpi=dpi)
+        self.canvas = FigureCanvas(self.fig)
+        self.canvas.setParent(self)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.vbox = QtWidgets.QVBoxLayout()
+        self.vbox.addWidget(self.toolbar)
+        self.vbox.addWidget(self.canvas)
+
+        self.setLayout(self.vbox)
+
+    def getFigure(self):
+        return self.fig
+
+    def draw(self):
+        self.canvas.draw()
